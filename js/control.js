472ff9b1c51b02fe16a8bd9d866628cfecb38ee8
Jamie Ridding <Themayu@users.noreply.github.com>
2017-03-20 15:40:04 +0000

    Build field editing code; begin work on code to save field

--- js/control.js
+++ js/control.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   var assembleSidebarServerEntry = function (index, el) {
-    var server = $('<div class="server"></div>');
-    $(server).append('<p class="name">' + el['server-name'] + '</p>');
+    var server = $('<div class="server py-1 px-2 mb-1" data-id="' + index + '"></div>');
+    $(server).append('<p class="name mb-1 mr-1 d-inline-block">' + el['server-name'] + '</p>');
+    $(server).append('<p class="user-count text-muted mb-1 d-inline-block">' + el['user-count'] + ' Users</p>')
+    $(server).append('<p class="short-desc m-0 d-block">' + truncateStr(el['description'], 50) + '</p>');
+    $(server).append('<p class="invite text-muted m-0 mt-1">(' + el['invite'] + ')</p>');
 
-    $('#sidebar-server-list').append('<p>Hello!</p>');
-
-    console.log(server);
+    serverList += server.get(0).outerHTML;
   }
 
   var exampleServerData = [
@@ -21,6 +22,7 @@ $(document).ready(function() {
       'rating': 1,
       'server-name': "Purgatory",
       'user-count': 260,
+      'invite': 'BeWdRTp',
       'example-messages': [
         {
           channel: '#general',
@@ -50,13 +52,41 @@ $(document).ready(function() {
     }
   ]
 
+  var showEditor = function(e) {
+    var $field = $($(this).data('field'));
+    var $staticField = $($field.get(0));
+    var $editor = $($field.get(1));
+
+    $(this).css({transform: 'translateY(-50%)'}).children('i').removeClass('fa-pencil').addClass('fa-floppy-o');
+    $staticField.removeClass('d-inline-block');
+    $editor.css({width: $staticField.width() + 'px', height: Math.ceil($staticField.height()*1.1)+2}).val($staticField.text()).addClass('d-inline-block');
+    console.log($field, $staticField, $editor, $editor.get(0).value.width);
+
+    $(this).off('click').click(saveAndHideEditor);
+  }
+
+  var saveAndHideEditor = function(e) {
+    $(this).off('click').click(showEditor);
+  }
+
+  var showServerPanel = function() {
+    var cp = $('#control-frame');
+    var id = parseInt($(this).data('id'));
+    cp.attr('data-index', id);
+
+    cp.find('.server-name').text(exampleServerData[id]['server-name']);
+
+    cp.css({display: 'block'});
+
+    cp.find('.server-name + .field-actions').click(showEditor);
+  }
+
   var serverList = '';
   var index = 0;
 
-  for (server of exampleServerData) {
-    assembleSidebarServerEntry(index, server);
-    ++index;
-  }
+  $.each(exampleServerData, assembleSidebarServerEntry);
 
   $('#sidebar-server-list').html(serverList);
+
+  $('.server', '#sidebar-server-list').click(showServerPanel);
 });
